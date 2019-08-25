70e574ddf379f6b226810baf162e0d27925620d6
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-08-25 19:53:11 -0300

    adicionando todos os arquivos do projeto

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+
+	var menu = document.getElementById("sub_menu");
+
+	if (menu.style.display == 'none' || menu.style.display == '') {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none"
+	}
+
+}
+function toggleMenu2() {
+
+	var menu2 = document.getElementById("sub_menu2");
+
+	if (menu2.style.display == 'none' || menu2.style.display == '') {
+		menu2.style.display = "block";
+	} else {
+		menu2.style.display = "none"
+	}
+
+}
