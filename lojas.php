8dde17c51726b0cda739dd34829c48c65678916d
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-09-10 13:07:50 -0300

    atualizando repositório com as mudanças feita na sala b01

---
+++ lojas.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Delicia Gelada   
+        </title>
+		<meta charset="utf-8">
+		<link type="text/css" rel="stylesheet" href="css/style.css">
+        <link rel="icon" href="icon/logo-main.png" type="image/x-icon" />
+    </head>
+    <body>
+    	<?php
+			require_once('modulos/header.php');
+		?>
+		<section id="localizador">
+            <div class="conteudo center">
+                <div class="caixa_texto_localizador">
+                    <h2 class="titulo2">
+                        ENCONTRE UMA LOJA
+                    </h2>
+                    <p class="texto_pequeno">
+                        Localize uma loja Delícia Gelada mais próxima à você.
+                    </p>
+                </div>
+                <div class="caixa_seletor_localizacao">
+                    
+                    <select id="seletor_estado">
+                        <option>ESTADO</option>
+                    </select>
+                    
+                     <select id="seletor_cidade">
+                        <option>CIDADE</option>
+                    </select>
+                    
+                </div>
+            </div>
+		</section>
+        <section id="lojas">
+            <div class="conteudo center">
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 1</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+                <div class="caixa_lojas">
+                    <h2 class="texto_card_loja">LOJA 2</h2>
+                    <div class="endereco_loja">
+                        <div class="linha_vert"></div>
+                        <div>Av.odin</div>
+                        <div>Bairro: Thor</div>
+                        <div>Cidade - UF</div>
+                        <div>333-666</div>
+                    </div>
+                    <button class="botao_localizar"> VER NO MAPA</button>
+                </div>
+            </div>
+        </section>
+		<?php
+			require_once('modulos/footer.php');
+		?>
+    </body>
+</html>
---
+++ modulos/footer.php
@@ -0,0 +1,22 @@
+<footer id="rodape">
+	<div class="conteudo center">
+		<div class="sistema_interno">
+			<button id="btn_sistema_interno">
+				Sistema Interno
+			</button>
+		</div>
+		<div class="endereco">
+			Endereço: XXXXXXXXXXX.XXXXXXX - XXXXXXXXX
+		</div>
+		<div class="aplicativo">
+			<div class="app_img">
+				<img src="icon/android.png" alt="app">
+			</div>
+			<div class="app_botao">
+				<button id="btn_app">
+					Baixe o App
+				</button>
+			</div>
+		</div>
+	</div>
+</footer>
